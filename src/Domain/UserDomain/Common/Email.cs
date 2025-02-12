3466fe7d523f84fe72aaac24701b09d259776183
Clark Leovison Rey <cleovison@gmail.com>
2025-02-13 00:19:40 +0800

    feat: Added Email to the ValueObject

---
+++ src/Domain/UserDomain/Common/Email.cs
@@ -0,0 +1,26 @@
+using System.Text.RegularExpressions;
+using Wait.Primitives;
+
+namespace Wait.Domain.UserDomain.Common;
+
+
+public class Email : ValueObject
+{
+
+    private static readonly Regex EmailRegex = new("^[a-zA-Z0-9._%±]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$");
+    public Email(string value)
+    {
+        if (!EmailRegex.IsMatch(value))
+        {
+            throw new ArgumentException("Your email address is not valid");
+        }
+
+        Value = value;
+    }
+
+    public string Value { get; init; }
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        throw new ArgumentException();
+    }
+}
--- src/Domain/UserDomain/User.cs
+++ src/Domain/UserDomain/User.cs
@@ -8,4 +8,6 @@ public class User
     public required Username Username { get; init; }
     public required Password Password { get; init; }
     public required FirstName FirstName { get; init; }
+    public required LastName LastName { get; init; }
+    public required Email Email { get; init; }
 }
