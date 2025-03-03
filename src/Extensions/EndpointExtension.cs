067ffb3bed3a340a95655dc13fe809594c81b914
Clark Leovison Rey <cleovison@gmail.com>
2025-03-04 00:10:20 +0800

    feat: Added EndpointExtension

--- src/Extensions/EndpointExtension.cs
+++ src/Extensions/EndpointExtension.cs
@@ -6,7 +6,7 @@ namespace Wait.Extensions;
 
 public static class EndpointExtensions
 {
-    public static IServiceCollection AddEndpoint(this IServiceCollection services, Assembly assembly)
+    public static IServiceCollection AddEndpoints(this IServiceCollection services, Assembly assembly)
     {
         ServiceDescriptor[] serviceDescriptors = assembly.DefinedTypes
         .Where(type => type is { IsAbstract: false, IsInterface: false } && type.IsAssignableFrom(typeof(IEndpoint)))
@@ -16,8 +16,16 @@ public static class EndpointExtensions
         return services;
     }
 
-    public static IApplicationBuilder Endpoint(this WebApplication app)
+    public static IApplicationBuilder Endpoint(this WebApplication app, RouteGroupBuilder? routeGroupBuilder = null)
     {
+        IEnumerable<IEndpoint> endpoints = app.Services.GetRequiredService<IEnumerable<IEndpoint>>();
+        IEndpointRouteBuilder builder = routeGroupBuilder is null ? app : routeGroupBuilder;
 
+        foreach (IEndpoint endpoint in endpoints)
+        {
+            endpoint.Endpoint(builder);
+        }
+
+        return app;
     }
 }
--- src/Program.cs
+++ src/Program.cs
@@ -1,13 +1,18 @@
+using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 using Wait.Database;
+using Wait.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(optionsBuilder =>
 {
     optionsBuilder.UseNpgsql(builder.Configuration["ConnectionStrings: DefaultConnection"]!);
 });
+builder.Services.AddEndpoints(Assembly.GetExecutingAssembly());
+
 
 var app = builder.Build();
+app.Endpoint();
 
 
 app.Run();
