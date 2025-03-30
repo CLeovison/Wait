5c989b5bf05124c293a49f66c4bdf722a115bb3b
Clark Leovison Rey <cleovison@gmail.com>
2025-03-30 23:57:34 +0800

    fix: Need to Add username,password and email into the configurations

--- src/Program.cs
+++ src/Program.cs
@@ -21,16 +21,22 @@ app.MapPost("/create", async (CreateUserRequest request, AppDbContext dbContext,
     User user = new()
     {
         UserId = Guid.NewGuid(),
+        Username = request.Username,
+        Password = request.Password,
+        Email = request.Email,
         FirstName = request.FirstName,
         LastName = request.LastName
-
-
     };
 
     dbContext.User.Add(user);
     await dbContext.SaveChangesAsync(cancellationToken);
 
     return Results.Ok();
+});
+app.MapGet("/", async (AppDbContext dbContext) =>
+{   var user = await dbContext.User.ToListAsync();
+    return Results.Ok();
+
 });
 app.Run();
 
