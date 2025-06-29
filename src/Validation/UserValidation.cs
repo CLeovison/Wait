5bbd5c0eebf91de9747b761400c96ba6b0f6ca41
Clark Leovison Rey <cleovison@gmail.com>
2025-06-29 23:04:04 +0800

    feat: Added Validation for User

--- src/Validation/UserValidation.cs
+++ src/Validation/UserValidation.cs
@@ -1,7 +1,24 @@
+
+using System.Data;
+using FluentValidation;
+using Wait.Domain.Entities;
+
 namespace Wait.Validation;
 
 
-public class UserValidationn
+public class UserValidation : AbstractValidator<Users>
 {
-
+    public UserValidation()
+    {
+        RuleFor(user => user.FirstName).NotEmpty().WithMessage("Please Fill the First Name");
+        RuleFor(user => user.LastName).NotEmpty().WithMessage("Please Fill the Last Name");
+        RuleFor(user => user.Username).MinimumLength(5).NotEmpty().WithMessage("The Minimum Length for Username is 5 Characters");
+        RuleFor(user => user.Password).NotEmpty().MinimumLength(8)
+        .WithMessage("Your Password length must be atleast 8")
+        .Matches(@"[A-Z]+").WithMessage("Your password must contain at least one uppercase letter.")
+        .Matches(@"[a-z]+").WithMessage("Your password must contain at least one lowercase letter.")
+        .Matches(@"[0-9]+").WithMessage("Your password must contain at least one number.")
+        .Matches(@"[\!\?\*\.]+").WithMessage("Your password must contain at least one (!? *.).");
+        RuleFor(user => user.Email).EmailAddress().NotEmpty().WithMessage("Please provide an email address");
+    }
 }
