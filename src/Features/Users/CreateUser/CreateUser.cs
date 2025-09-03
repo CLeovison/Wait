93ea6b787ab9f3378590c586b815ff8e80ef9e10
Clark Leovison Rey <cleovison@gmail.com>
2025-09-03 13:57:46 +0800

    refactor: Refactoring the Architecture from Clean Architecture to Vertical Slice

---
+++ src/Features/Users/CreateUser/CreateUser.cs
@@ -0,0 +1,15 @@
+using Wait.Abstract;
+using Wait.Contracts.Data;
+
+namespace Wait.Features.Users.Create;
+
+
+public record CreateUserRequest(string FirstName, string LastName, string Username, string Password, string Email);
+public record CreateUserResponse(string status, string message, List<UserDto> data);
+public sealed class CreateUser : IEndpoint
+{
+    public void Endpoint(IEndpointRouteBuilder app)
+    {
+
+    }
+}
