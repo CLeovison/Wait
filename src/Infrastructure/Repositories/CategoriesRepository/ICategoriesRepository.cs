2af0941c1c462f71d1e75c0276e506336034f4d1
Clark Leovison Rey <cleovison@gmail.com>
2025-09-06 02:15:29 +0800

    feat: Added New Interface for CategoriesRepository

---
+++ src/Infrastructure/Repositories/CategoriesRepository/ICategoriesRepository.cs
@@ -0,0 +1,13 @@
+using Wait.Domain.Entities;
+
+namespace Wait.Infrastructure.Repositories.CategoriesRepository;
+
+
+public interface ICategoriesRepository
+{
+    Task<Category> CreateCategoriesAsync(Category category, CancellationToken ct);
+    Task<Category> GetCategoryByIdAsync(Guid id, CancellationToken ct);
+    Task<(List<Category>, int totalCount)> GetAllCategoryAsync(string searchTerm, CancellationToken ct);
+    Task<Category> UpdateCategoryAsync(Category category, CancellationToken ct);
+    Task<bool> DeleteCategoryAsync(Guid id, CancellationToken ct);
+}
