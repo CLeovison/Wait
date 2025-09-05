35f962f18e43223040c8e6d2e55355c6f2afae8c
Clark Leovison Rey <cleovison@gmail.com>
2025-09-05 15:56:39 +0800

    feat: Added Auth on the Features

--- src/Features/Users/CreateUser/CreateUser.cs
+++ src/Features/Users/CreateUser/CreateUser.cs
@@ -6,6 +6,7 @@ namespace Wait.Features.Users.Create;
 
 public record CreateUserRequest(string FirstName, string LastName, string Username, string Password, string ConfirmPassowrd, string Email);
 public record CreateUserResponse(string status, string message, List<UserDto> data);
+
 public sealed class CreateUser : IEndpoint
 {
     public void Endpoint(IEndpointRouteBuilder app)
@@ -13,3 +14,4 @@ public sealed class CreateUser : IEndpoint
 
     }
 }
+
