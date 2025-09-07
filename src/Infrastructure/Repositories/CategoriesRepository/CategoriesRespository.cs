31589422cd17a0981b383020335802a2081bcea9
Clark Leovison Rey <cleovison@gmail.com>
2025-09-07 17:47:59 +0800

    feat: Added the implementation of all of repository in categories

--- src/Infrastructure/Repositories/CategoriesRepository/CategoriesRespository.cs
+++ src/Infrastructure/Repositories/CategoriesRepository/CategoriesRespository.cs
@@ -14,4 +14,28 @@ public sealed class CategoriesRepository(AppDbContext dbContext) : ICategoriesRe
 
         return category;
     }
+
+    public async Task<Category?> GetCategoryByIdAsync(Guid id, CancellationToken ct)
+    {
+        return await dbContext.Category.FindAsync(id);
+    }
+
+    public async Task<(List<Category>, int totalCount)> GetAllCategoryAsync(string? searchTerm, string sortBy, bool desc,  int skip,
+     int take, CancellationToken ct)
+    {
+
+    }
+    public async Task<Category> UpdateCategoryAsync(Category category, CancellationToken ct)
+    {
+        dbContext.Category.Update(category);
+        await dbContext.SaveChangesAsync();
+        return category;
+    }
+
+    public async Task<bool> DeleteCategoryAsync(Category category, CancellationToken ct)
+    {
+        dbContext.Category.Remove(category);
+        await dbContext.SaveChangesAsync();
+        return category is not null;
+    }
 }
--- src/Infrastructure/Repositories/CategoriesRepository/ICategoriesRepository.cs
+++ src/Infrastructure/Repositories/CategoriesRepository/ICategoriesRepository.cs
@@ -6,8 +6,9 @@ namespace Wait.Infrastructure.Repositories.CategoriesRepository;
 public interface ICategoriesRepository
 {
     Task<Category> CreateCategoriesAsync(Category category, CancellationToken ct);
-    Task<Category> GetCategoryByIdAsync(Guid id, CancellationToken ct);
-    Task<(List<Category>, int totalCount)> GetAllCategoryAsync(string searchTerm, CancellationToken ct);
+    Task<Category?> GetCategoryByIdAsync(Guid id, CancellationToken ct);
+    Task<(List<Category>, int totalCount)> GetAllCategoryAsync(string? searchTerm, string sortBy, bool desc, int skip,
+     int take, CancellationToken ct);
     Task<Category> UpdateCategoryAsync(Category category, CancellationToken ct);
     Task<bool> DeleteCategoryAsync(Guid id, CancellationToken ct);
 }
