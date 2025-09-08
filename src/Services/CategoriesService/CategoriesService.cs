7322ee40fb02251dc70fb86d1b153aee22aa5e16
Clark Leovison Rey <cleovison@gmail.com>
2025-09-08 23:53:52 +0800

    feat: Added a try catch block for the CreateCategories

--- src/Services/CategoriesService/CategoriesService.cs
+++ src/Services/CategoriesService/CategoriesService.cs
@@ -9,9 +9,29 @@ public sealed class CategoriesService(ICategoriesRepository categoriesRepository
 {
     public async Task<CategoryDto> CreateCategoryAsync(CategoryDto category, CancellationToken ct)
     {
-        var categoryMap = category.ToCreate();
-        var categoryRequest = await categoriesRepository.CreateCategoriesAsync(categoryMap, ct);
-        var categoryEntity = categoryRequest.ToDto();
-        return categoryEntity;
+        string message = $"The {category.CategoryName} was already exisiting";
+
+        if (category is not null)
+        {
+            throw new InvalidOperationException(message);
+        }
+        try
+        {
+            var categoryMap = category?.ToCreate();
+            var categoryRequest = await categoriesRepository.CreateCategoriesAsync(categoryMap, ct);
+            var categoryEntity = categoryRequest.ToDto();
+            return categoryEntity;
+        }
+        catch (Exception ex)
+        {
+            throw new ArgumentException("Exception", ex);
+        }
+
+    }
+
+    public async Task<CategoryDto> GetCategoryByIdAsync(Guid id, CancellationToken ct)
+    {
+        
+
     }
 }
--- src/Services/CategoriesService/ICategoriesService.cs
+++ src/Services/CategoriesService/ICategoriesService.cs
@@ -6,4 +6,5 @@ namespace Wait.Services.Categories;
 public interface ICategoriesService
 {
     Task<CategoryDto> CreateCategoryAsync(CategoryDto category, CancellationToken ct);
+    Task<CategoryDto> GetCategoryByIdAsync(Guid id, CancellationToken ct);
 }
