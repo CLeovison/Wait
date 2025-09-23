a0a357fc20cae23830abd23d878c354a24779cf6
Clark Leovison Rey <cleovison@gmail.com>
2025-09-23 23:01:37 +0800

    refactor: Replace the Results in the CommandHandler of Result based on Result DesignPattern

--- src/Abstract/Messaging/ICommandHandler.cs
+++ src/Abstract/Messaging/ICommandHandler.cs
@@ -1,6 +1,8 @@
+using Wait.Abstract.Results;
+
 namespace Wait.Abstract.Messaging;
 
 public interface ICommandHandler<in TCommand> where TCommand : ICommand
 {
-    Task<Results> Handle(TCommand command, CancellationToken ct);
+    Task<Result> Handle(TCommand command, CancellationToken ct);
 }
