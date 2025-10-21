84a4b26d0b1e040da4b05a7ef678c82553ee434d
Clark Leovison Rey <cleovison@gmail.com>
2025-10-22 01:41:09 +0800

    feat: Added ImageEndpoint for Upload,Status and Getting the ID for async process

--- src/Services/ProductService/ProductService.cs
+++ src/Services/ProductService/ProductService.cs
@@ -20,7 +20,7 @@ IConfiguration configuration) : IProductService
 {
 
     private readonly string _uploadDirectory = configuration["UploadDirectory : UploadFolder"] ?? "Uploads";
-    public async Task<ProductDto> CreateProductAsync(CreateProductWithImageRequest product, CancellationToken ct)
+    public async Task<ProductDto> CreateProductAsync(ProductDto product, CancellationToken ct)
     {
         
         var normalizedCategory = product.CategoryName.Trim();
@@ -31,10 +31,6 @@ IConfiguration configuration) : IProductService
             throw new ArgumentNullException("The Category does not exist, please add this shit");
         }
 
-
-        string? imagePath = string.Empty;
-
-        if()
         var createProduct = product.ToCreate(category.CategoryId);
 
         var request = await productRepository.CreateProductAsync(createProduct, ct);
