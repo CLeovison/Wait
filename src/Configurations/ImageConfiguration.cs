9ac80b055c489fb21726fa304aaf801d725784e3
Clark Leovison Rey <cleovison@gmail.com>
2025-12-24 19:03:15 +0800

    modified : Image Configurations

--- src/Configurations/ImageConfiguration.cs
+++ src/Configurations/ImageConfiguration.cs
@@ -8,17 +8,49 @@ public sealed class ImageConfiguration : IEntityTypeConfiguration<ImageResult>
 {
   public void Configure(EntityTypeBuilder<ImageResult> builder)
   {
-    builder.HasKey(x => x.ObjectKey);
+    builder.HasKey(x => x.ImageId);
 
-    builder.Property(x => x.ObjectKey).IsRequired();
-    builder.Property(x => x.StorageUrl).IsRequired();
-    builder.Property(x => x.MimeType).IsRequired();
-    builder.Property(x => x.FileExtension).IsRequired();
-    builder.Property(x => x.OriginalFileName).IsRequired();
-    builder.Property(x => x.FileLength);
-    builder.Property(x => x.DateUploaded).ValueGeneratedOnAdd().HasDefaultValueSql("current_date");
-    builder.Property(x => x.DateModified).ValueGeneratedOnUpdate().HasDefaultValueSql("current_date");
-    builder.Property(x => x.UserId).IsRequired();
-    builder.HasOne(x => x.Product).WithMany(x => x.ImageUrl).IsRequired();
+    builder.Property(x => x.ImageId)
+           .ValueGeneratedNever();
+
+    builder.Property(x => x.ObjectKey)
+           .IsRequired()
+           .HasMaxLength(512);
+
+    builder.Property(x => x.StorageUrl)
+           .IsRequired()
+           .HasMaxLength(2048);
+
+    builder.Property(x => x.MimeType)
+           .IsRequired()
+           .HasMaxLength(100);
+
+    builder.Property(x => x.FileExtension)
+           .IsRequired()
+           .HasMaxLength(20);
+
+    builder.Property(x => x.OriginalFileName)
+           .IsRequired()
+           .HasMaxLength(255);
+
+    builder.Property(x => x.FileLength)
+           .IsRequired();
+
+    builder.Property(x => x.DateUploaded)
+           .HasDefaultValueSql("now()")
+           .ValueGeneratedOnAdd();
+
+    builder.Property(x => x.DateModified)
+           .HasDefaultValueSql("now()")
+           .ValueGeneratedOnAddOrUpdate();
+
+    builder.HasIndex(x => x.ObjectKey).IsUnique();
+    builder.HasIndex(x => x.ProductId);
+    builder.HasIndex(x => x.UserId);
+
+    builder.HasOne(x => x.Product)
+           .WithMany(p => p.ImageUrl)
+           .HasForeignKey(x => x.ProductId)
+           .OnDelete(DeleteBehavior.Cascade);
   }
 }
